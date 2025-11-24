4cba7d0b75dcf344571bbc2ce83272193db02032
Nikita <suicidebenz@yandex.ru>
2025-11-24 23:33:54 +0700

    init commit

---
+++ src/test/java/delivery/system/DeliverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package delivery.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
